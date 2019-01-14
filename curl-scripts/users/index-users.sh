002804fd6a49c74492a01215ed89f078f2219ada
candydasein <michaellorensiegel@gmail.com>
2019-01-14 16:14:16 -0500

    user curl scripts working

---
+++ curl-scripts/users/index-users.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#TOKEN=BAhJIiVlZjYxYTQzNzk3OTIwZTZhYjZhZTAyNDZiNzVmMWY5OAY6BkVG--ba768896f518af21be9216a7f7edb4c0a7f90849 sh curl-scripts/users/index-users.sh
+curl "http://localhost:4741/users" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
