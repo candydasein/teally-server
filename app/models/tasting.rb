755664bb6e36894ccd8274b970dcbe5c5e9d5584
candydasein <michaellorensiegel@gmail.com>
2019-01-23 14:06:02 -0500

    delete fixed by adding dependent: :destroy to tastings model

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -1,6 +1,6 @@
 class Tasting < ApplicationRecord
   belongs_to :user
   belongs_to :tea
-  has_many :flavor_likes
+  has_many :flavor_likes, dependent: :destroy
   has_many :flavors, through: :flavor_likes
 end
